3e19841ce3620b8cc0c3f4b36c4485efe974aef4
johnfinley <johnfinleybremerton@gmail.com>
2024-04-29 14:59:31 -0600

    UPDATE: changed file structure to work with deploy requirements

--- app.py
+++ app.py
@@ -3,41 +3,25 @@ import os
 from flask import Flask, render_template
 from services import is_double_yesterday
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    @app.route('/')
-    def index():
-        return render_template('double/index.html')
+@app.route('/')
+def index():
+    return render_template('double/index.html')
 
-    @app.route('/_internal/get_double_data')
-    def get_double_data():
-        result = is_double_yesterday()
-        return result
-
-    return app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(
-        host="0.0.0.0",
-        port=8000,
-    )
+@app.route('/_internal/get_double_data')
+def get_double_data():
+    result = is_double_yesterday()
+    return result
