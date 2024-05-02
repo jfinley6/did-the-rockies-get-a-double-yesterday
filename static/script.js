7be57806b31ba79949fbad7a9be5cca8f08791fe
johnfinley <johnfinleybremerton@gmail.com>
2024-05-01 21:20:22 -0600

    UPDATE: Refactored HTML and CSS for improved visual consistency and aesthetics

--- static/script.js
+++ static/script.js
@@ -1,29 +1,31 @@
 // Generate HTML using double data
 function generateHTML(data) {
     let container = document.querySelector(".container");
-    let placeholderImage = document.querySelector(".placeholder-image")
-    placeholderImage.remove()
-    document.body.style.display = "flex";
+    // let placeholderImage = document.querySelector(".placeholder-image")
+    // placeholderImage.remove()
+    // document.body.style.display = "flex";
+    container.innerHTML = ''
 
     let htmlData =
         '<div class="content">' +
-        "<h1>Did The Rockies Get a Double Yesterday?</h1>" +
-        '<div class="double-answer">' +
-        `${data.answer}` +
-        `<h2>${data.details}</h2>` +
-        "</div>" +
-        `<h2>${data.moreDetails}</h2>` +
+        '<p class="title">Did The Rockies Get a Double Yesterday?</p>' +
+        '<div class="line"></div>' +
+        `<p class="answer">${data.answer}</p>` +
+        `<p class="details">${data.details}</p>` +
+        '<div class="line"></div>' +
+        `<p class="moreDetails">${data.moreDetails}</p>` +
         "</div>" +
         "<footer>" +
-        "<h4>" +
+        '<p class="footer-message">' +
         "This website was built for fun and is an " +
         '<a target="_blank" href="https://github.com/jfinley6/did-the-rockies-get-a-double-yesterday"' +
         ">Open Source Project</a>" +
-        "</h4>" +
-        "<h4>" +
-        "Created and maintained by " +
-        '<a target="_blank" href="https://www.linkedin.com/in/john-tyler-finley/">John Finley</a>' +
-        "</h4>" +
+        "</p>" +
+        '<p class="footer-message">' +
+        "Created and maintained by  " +
+        '<a target="_blank" href="https://www.linkedin.com/in/john-tyler-finley/"' +
+        ">John Finley</a>" +
+        "</p>" +
         "</footer>";
 
     container.innerHTML += htmlData;
@@ -49,24 +51,24 @@ function saveToLocalStorage(data) {
 // If current day double results already exist then just use localstorage, otherwise fetch from server
 function checkLocalStorage() {
     const storedUserData = JSON.parse(localStorage.getItem("doubleData"));
-    const currentDate = new Date().toLocaleDateString()
+    const currentDate = new Date().toLocaleDateString();
     if (storedUserData !== null && storedUserData.dateAccessed === currentDate) {
         return Promise.resolve(storedUserData);
     } else {
         return fetch("/_internal/get_double_data")
             .then((response) => response.json())
             .then((data) => {
-                saveToLocalStorage(data)
+                saveToLocalStorage(data);
                 return data;
             })
             .catch((error) => {
-                console.log(error)
-            })
+                console.log(error);
+            });
     }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     checkLocalStorage().then((userData) => {
-        generateHTML(userData)
-    })
+        generateHTML(userData);
+    });
 });
